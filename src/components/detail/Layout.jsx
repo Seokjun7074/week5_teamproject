e75dcd640832e4d1db63fb61b5e95cbe2005a7a6
honeyoung <dudgns5845@naver.com>
2022-08-06 03:01:08 +0900

    #4 리뷰 뷰 추가

--- src/components/detail/Layout.jsx
+++ src/components/detail/Layout.jsx
@@ -1,17 +1,52 @@
 import React from "react";
 import styled from "styled-components";
+
 const Layout = (props) => {
   return (
     <MainWrap>
-      <button>카페조아</button>
+      <Header>
+        <Button>카페조아</Button>
+        <Button>마이페이지</Button>
+        <Button>로그아웃</Button>
+      </Header>
       <ContentWrap>
-        <Image></Image>
-        <InputField type={"text"} value={"카페명"} />
-        <InputField type={"text"} value={"카페 주소"} />
-        <InputField type={"text"} value={"카페 전화번호"} />
-        <InputField type={"text"} value={"카페 영업시간"} />
-        <Button>저장</Button>
+        <CafeImage src="https://i.pinimg.com/564x/de/cb/ff/decbff83d65b9bfbbbd3a3ad68420414.jpg" />
+        <CafeInfo>
+          <div>카페명 : {"숭실마루"}</div>
+          <div>카페명 : {"서울특별시 동작구 XXX"}</div>
+          <div>카페명 : {"02-XXX-XXXX"}</div>
+          <div>카페명 : {"07시~20시"}</div>
+        </CafeInfo>
       </ContentWrap>
+
+      <ReviewWrap>
+        <div>
+          <input type={"text"} placeholder="후기를 남겨주세요!"></input>
+          <button>등록</button>
+        </div>
+        <div>
+          <Review>
+            <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+            <div>카페가 정말 이쁩니다!!</div>
+          </Review>
+          <Review>
+            <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+            <div>카페가 정말 이쁩니다!!</div>
+          </Review>
+          <Review>
+            <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+            <div>카페가 정말 이쁩니다!!</div>
+          </Review>
+          <Review>
+            <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+            <div>카페가 정말 이쁩니다!!</div>
+          </Review>
+          <Review>
+            <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+            <div>카페가 정말 이쁩니다!!</div>
+          </Review>
+        </div>
+      </ReviewWrap>
     </MainWrap>
   );
 };
@@ -19,44 +54,71 @@ const Layout = (props) => {
 export default Layout;
 
 const MainWrap = styled.div`
-  min-width: 800px;
-  min-width: 1500px;
-  height: 800px;
-  background-color: orange;
+  /* background-color: orange; */
+  height: 100%;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
 const ContentWrap = styled.div`
-  width: 500px;
-  height: 700px;
-  background-color: red;
-  border: 2px solid;
+  width: 100%;
+  height: 100%;
+  /* background-color: red; */
+
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
   padding: 50px;
+  justify-content: space-evenly;
 `;
-const Image = styled.div`
-  width: 300px;
-  height: 200px;
+const CafeImage = styled.img`
+  width: 720px;
+  height: 480px;
   background-color: green;
   margin-bottom: 30px;
+  object-fit: cover;
+  border: none;
+  border-radius: 30px;
 `;
 
-const InputField = styled.input`
-  background-color: blue;
-  width: 400px;
+const CafeInfo = styled.div`
+  text-align: left;
+  div {
+    margin-bottom: 50px;
+    font-size: ${(props) => props.theme.fontSize.button};
+  }
+`;
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: #ca965c;
+  align-items: center;
+  justify-content: space-around;
+
   height: 50px;
-  margin-top: 30px;
-  color: white;
+  width: 100%;
 `;
-
 const Button = styled.button`
-  background-color: greenyellow;
-  color: white;
+  /* background-color: greenyellow; */
+  color: black;
   width: 100px;
   height: 30px;
-  margin-top: 100px;
+`;
+
+const ReviewWrap = styled.div`
+  /* background-color: blue; */
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
+
+const Review = styled.div`
+  width: 800px;
+  height: 100px;
+  border: 2px solid;
+  border-radius: 30px;
+  padding: 20px 50px;
+  margin-bottom: 50px;
 `;
