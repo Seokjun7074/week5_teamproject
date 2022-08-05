cb7773d4cc960d312ab03fff35892e47cff22a60
SUNNY-LAPTOP\Sunny <lovysunny7@gmail.com>
2022-08-05 22:11:07 +0900

    [Test] Add react-scripts to run test, modified some files automatically

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import theme from "./style/theme";
 import Router from "./Router";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router />
+      <Login/>
     </ThemeProvider>
   );
 }
