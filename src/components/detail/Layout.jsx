fa75f50ad23def7b13bc8ba2ab6be57e9d8968d4
honeyoung <dudgns5845@naver.com>
2022-08-06 20:03:38 +0900

    #4  수정 버튼 추가

--- src/components/detail/Layout.jsx
+++ src/components/detail/Layout.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const Layout = (props) => {
+  const now = new Date().toISOString().substring(0, 10);
+
   return (
     <MainWrap>
       <Header>
@@ -12,6 +14,7 @@ const Layout = (props) => {
       <ContentWrap>
         <CafeImage src="https://i.pinimg.com/564x/de/cb/ff/decbff83d65b9bfbbbd3a3ad68420414.jpg" />
         <CafeInfo>
+          <button>수정</button>
           <div>카페명 : {"숭실마루"}</div>
           <div>카페명 : {"서울특별시 동작구 XXX"}</div>
           <div>카페명 : {"02-XXX-XXXX"}</div>
@@ -26,23 +29,52 @@ const Layout = (props) => {
         </div>
         <div>
           <Review>
-            <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+            <ExitButton>X</ExitButton>
+            <div style={{ display: "flex" }}>
+              <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+              <div style={{ marginLeft: "10px" }}>{now}</div>
+            </div>
             <div>카페가 정말 이쁩니다!!</div>
           </Review>
           <Review>
-            <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+            <ExitButton>X</ExitButton>
+            <div style={{ display: "flex" }}>
+              <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+              <div style={{ marginLeft: "10px" }}>{now}</div>
+            </div>
             <div>카페가 정말 이쁩니다!!</div>
           </Review>
           <Review>
-            <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+            <ExitButton>X</ExitButton>
+            <div style={{ display: "flex" }}>
+              <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+              <div style={{ marginLeft: "10px" }}>{now}</div>
+            </div>
             <div>카페가 정말 이쁩니다!!</div>
           </Review>
           <Review>
-            <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+            <ExitButton>X</ExitButton>
+            <div style={{ display: "flex" }}>
+              <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+              <div style={{ marginLeft: "10px" }}>{now}</div>
+            </div>
             <div>카페가 정말 이쁩니다!!</div>
           </Review>
           <Review>
-            <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+            <ExitButton>X</ExitButton>
+            <div style={{ display: "flex" }}>
+              <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+              <div style={{ marginLeft: "10px" }}>{now}</div>
+            </div>
+            <div>카페가 정말 이쁩니다!!</div>
+          </Review>
+
+          <Review>
+            <ExitButton>X</ExitButton>
+            <div style={{ display: "flex" }}>
+              <div style={{ color: "red", textAlign: "left" }}>dudgns5845</div>
+              <div style={{ marginLeft: "10px" }}>{now}</div>
+            </div>
             <div>카페가 정말 이쁩니다!!</div>
           </Review>
         </div>
@@ -121,4 +153,17 @@ const Review = styled.div`
   border-radius: 30px;
   padding: 20px 50px;
   margin-bottom: 50px;
+  position: relative;
+`;
+
+const ExitButton = styled.div`
+  z-index: 1;
+  position: absolute;
+  color: white;
+  background-color: red;
+  border: none;
+  border-radius: 5px;
+  width: 20px;
+  height: 20px;
+  left: 95%;
 `;
