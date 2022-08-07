7fb6772eb1c3da22edcb1e2c26052a9d31af95ab
이석준 <sukjune96@gmail.com>
2022-08-07 16:05:35 +0900

    feat: 메인페이지 라우터 적용

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -11,6 +11,12 @@ import { Link, useNavigate } from "react-router-dom";
 const Header = () => {
   const navigate = useNavigate();
 
+  const goToMyPage = () => {
+    navigate("/mypage");
+  };
+  const goToLogin = () => {
+    navigate("/login");
+  };
   return (
     <HeaderWrapper>
       <Navigator>
@@ -18,8 +24,8 @@ const Header = () => {
           <div>로고</div>
         </Link>
         <NavRight>
-          <NavMypage>마이페이지</NavMypage>
-          <NavLogin>로그인/회원가입</NavLogin>
+          <NavMypage onClick={goToMyPage}>마이페이지</NavMypage>
+          <NavLogin onClick={goToLogin}>로그인/회원가입</NavLogin>
         </NavRight>
       </Navigator>
     </HeaderWrapper>
