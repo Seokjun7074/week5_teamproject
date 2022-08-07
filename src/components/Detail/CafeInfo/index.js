ff516916915f00322d86a8facc1119c5c76fa7ee
honeyoung <dudgns5845@naver.com>
2022-08-07 23:43:16 +0900

    #4 refactor
    - 카페 정보 Edit 기능 수정
    - 수정한 정보를 저장가능

--- src/components/Detail/CafeInfo/index.js
+++ src/components/Detail/CafeInfo/index.js
@@ -6,45 +6,54 @@ const CafeInfo = (props) => {
 
     const [isEdit, SetEdit] = useState(false);
 
-    const CafeData = {
-        title: "숭실마루",
-        address: "서울특별시 동작구 XXX",
-        phone: "02-XXX-XXXX",
-        time: "07시~20시",
-        image: "https://i.pinimg.com/564x/de/cb/ff/decbff83d65b9bfbbbd3a3ad68420414.jpg"
-    }
+
+    const [title, SetTitle] = useState("숭실마루");
+    const [address, SetAddress] = useState("서울특별시 동작구 XXX");
+    const [phone, SetPhone] = useState("02-XXX-XXXX");
+    const [time, SetTime] = useState("07시~20시");
+    const [image, SetImage] = useState("https://i.pinimg.com/564x/de/cb/ff/decbff83d65b9bfbbbd3a3ad68420414.jpg");
+
+
+    // const CafeData = {
+    //     title: "숭실마루",
+    //     address: "서울특별시 동작구 XXX",
+    //     phone: "02-XXX-XXXX",
+    //     time: "07시~20시",
+    //     image: "https://i.pinimg.com/564x/de/cb/ff/decbff83d65b9bfbbbd3a3ad68420414.jpg"
+    // }
 
     const onClickEditBtn = (e) => {
+        if (isEdit) alert("수정하시겠습니까?");
         SetEdit(!isEdit);
         e.target.innerText = isEdit ? "수정" : "저장";
     }
     return (
         <MainWrap>
-            <CafeImage src={CafeData.image} />
+            <CafeImage src={image} />
             <CafeInfoWrap>
                 <EditButton onClick={onClickEditBtn} >수정</EditButton>
                 <div>
                     <span>카페 명 : </span>
                     {
-                        !isEdit ? <span>{CafeData.title}</span> : <input type={"text"} placeholder={CafeData.title} />
+                        !isEdit ? <span>{title}</span> : <input type={"text"} placeholder={title} onChange={(e) => SetTitle(e.target.value)} />
                     }
                 </div>
                 <div>
                     <span>카페 주소 : </span>
                     {
-                        !isEdit ? <span>{CafeData.address}</span> : <input type={"text"} placeholder={CafeData.address} />
+                        !isEdit ? <span>{address}</span> : <input type={"text"} placeholder={address} onChange={(e) => SetAddress(e.target.value)} />
                     }
                 </div>
                 <div>
                     <span>전화 번호 : </span>
                     {
-                        !isEdit ? <span>{CafeData.phone}</span> : <input type={"text"} placeholder={CafeData.phone} />
+                        !isEdit ? <span>{phone}</span> : <input type={"text"} placeholder={phone} onChange={(e) => SetPhone(e.target.value)} />
                     }
                 </div>
                 <div>
                     <span>영업 시간 : </span>
                     {
-                        !isEdit ? <span>{CafeData.time}</span> : <input type={"text"} placeholder={CafeData.time} />
+                        !isEdit ? <span>{time}</span> : <input type={"text"} placeholder={time} onChange={(e) => SetTime(e.target.value)} />
                     }
                 </div>
             </CafeInfoWrap>
