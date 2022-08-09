632f89982e0767d3db6bbb2c7ac5c387fce8a3ae
honeyoung <dudgns5845@naver.com>
2022-08-10 00:43:59 +0900

    #4 코드 정리

--- src/components/Detail/CafeInfo/index.js
+++ src/components/Detail/CafeInfo/index.js
@@ -14,14 +14,6 @@ const CafeInfo = (props) => {
     const [image, SetImage] = useState("https://i.pinimg.com/564x/de/cb/ff/decbff83d65b9bfbbbd3a3ad68420414.jpg");
     const [heart, SetHeartCount] = useState(1234);
 
-    // const CafeData = {
-    //     title: "숭실마루",
-    //     address: "서울특별시 동작구 XXX",
-    //     phone: "02-XXX-XXXX",
-    //     time: "07시~20시",
-    //     image: "https://i.pinimg.com/564x/de/cb/ff/decbff83d65b9bfbbbd3a3ad68420414.jpg"
-    // }
-
     const onClickEditBtn = (e) => {
         if (isEdit) alert("수정하시겠습니까?");
         SetEdit(!isEdit);
