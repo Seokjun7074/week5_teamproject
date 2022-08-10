c4dbaf331f4b60dc9fc854a82a8bd7b7ba2c4d90
SUNNY-LAPTOP\Sunny <lovysunny7@gmail.com>
2022-08-10 15:07:47 +0900

    Feat: Add reducer setting in login, join (just link using dmmy)

---
+++ src/redux/modules/userSlice.js
@@ -0,0 +1,34 @@
+import { createSlice} from "@reduxjs/toolkit";
+
+//  action은 FSA(Flux Standard Action) 
+// type: 액션의 성질, payload: 액션의 인자, error, meta : 부가적 내용
+const initialState = {
+  userList: [],
+  tmpUser: {},
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    userJoin: (state, action) => {
+      state.tmpUser = action.payload;
+      console.log(state.tmpUser);
+      state.userList.push(state.tmpUser);
+      // console.log(state.userList[0]);
+    },
+
+    userLogin: (state, action) => {
+      state.tmpUser = action.payload;
+      console.log(state.tmpUser);
+    },
+  },
+  // extraReducers:(builder) =>{
+  //      builder
+  //      .addCase(join.pending, (state, action) => {
+  //       state.isJoining = true;
+  //     })}
+});
+
+export const { userJoin, userLogin } = userSlice.actions;
+export default userSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cafeReducer from "./modules/cafeSlice";
 import reviewReducer from "./modules/reviewSlice";
+import userReducer from "./modules/userSlice";
 
 const store = configureStore({
   reducer: {
     cafe: cafeReducer,
     review: reviewReducer,
+    user: userReducer,
   },
 });
 
