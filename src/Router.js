6e612937614516a13417346fa4ea74f280309b24
honeyoung <dudgns5845@naver.com>
2022-08-10 17:37:49 +0900

    #1 refactor 코드 수정

--- src/Router.js
+++ src/Router.js
@@ -4,7 +4,7 @@ import Join from "./pages/login/Join";
 import Login from "./pages/login/Login";
 import Main from "./pages/main/Main";
 
-import MyPage from "./pages/myPage/MyPage";
+import MyPage from "./pages/mypage/MyPage";
 
 
 
@@ -17,7 +17,7 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/join" element={<Join />} />
-        <Route exact path="/mypage" element={<MyPage />} />
+        <Route exact path="/mypage/:user_id" element={<MyPage />} />
         <Route exact path="/detail/:cafe_id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
