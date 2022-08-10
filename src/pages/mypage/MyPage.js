e23a0849ac046a6dadf1d7e18b4ba4eedebcdb80
이석준 <sukjune96@gmail.com>
2022-08-10 15:13:35 +0900

    style: 경로오타수정

--- src/pages/mypage/MyPage.js
+++ src/pages/mypage/MyPage.js
@@ -1,20 +1,18 @@
 import React from "react";
-import Head from "../../components/mypage/Head";
-import Posting from "../../components/mypage/Posting";
-import Profile from "../../components/mypage/Profile";
+import Head from "../../components/myPage/Head";
+import Posting from "../../components/myPage/Posting";
+import Profile from "../../components/myPage/Profile";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 
-
 export default function MyPage() {
-   
-    const tmpData=useSelector(state=>state.tmp.tmp);
-    console.log(tmpData);
-    return (
-        <div>
-            <Head />
-            <Profile tmpData={tmpData}/>
-            <Posting/>
-        </div>);
+  const tmpData = useSelector((state) => state.tmp.tmp);
+  console.log(tmpData);
+  return (
+    <div>
+      <Head />
+      <Profile tmpData={tmpData} />
+      <Posting />
+    </div>
+  );
 }
-
